966378985b7307371726061b541a82707e9b8f21
Aaron Parsons <aparsons@astron.berkeley.edu>
2009-01-08 23:24:55 -0400

    Modified print_params() in fit.py to not print keys for antennas/sources that do not have parameters being fit.

--- src/fit.py
+++ src/fit.py
@@ -52,6 +52,7 @@ def print_params(prms, indent='', grad=None):
     keys.sort()
     for k in keys:
         v = prms[k]
+        if v == {} or v is None or v == []: continue
         if type(v) == dict:
             print indent, k
             if grad is None: print_params(v, indent + '  ')
