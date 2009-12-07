941602d682d262017405cee4da5ba02517d450d1
Aaron Parsons <aparsons@astron.berkeley.edu>
2009-12-07 14:56:58 -0800

    Removed xrfi "infile/outfile" functionality and added skip_phs parameter to mk_img.  Incremented version to 0.9.1.

--- scripts/mk_img.py
+++ scripts/mk_img.py
@@ -25,6 +25,8 @@ o.add_option('--cnt', dest='cnt', type='int', default=0,
     help='Start counting output images from this number.  Default 0.')
 o.add_option('--fmt', dest='fmt', default='im%04d',
     help='A format string for counting successive images written to files.  Default is im%04d (i.e. im0001).')
+o.add_option('--skip_phs', dest='skip_phs', action='store_true',
+    help='Do not phase visibilities before gridding.')
 o.add_option('--skip_amp', dest='skip_amp', action='store_true',
     help='Do not use amplitude information to normalize visibilities.')
 o.add_option('--skip_bm', dest='skip_bm', action='store_true',
@@ -197,7 +199,7 @@ for srccnt, s in enumerate(cat.values()):
           d,f = d.take(chans), f.take(chans)
           if not opts.skip_amp: d /= aa.passband(i,j)
           # Throws PointingError if not up:
-          d = aa.phs2src(d, s, i, j)
+          if not opts.skip_phs: d = aa.phs2src(d, s, i, j)
           u,v,w = aa.gen_uvw(i,j,src=s)
           longenough = n.where(n.sqrt(u**2+v**2) < opts.minuv, 0, 1).squeeze()
           if not opts.skip_bm:
