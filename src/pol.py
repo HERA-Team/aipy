1dd911041d92b671eb410e9dbd76c3385bc7efe0
David Moore <damo@sas.upenn.edu>
2012-07-16 11:58:52 -0400

    Added antenna-based Jones matrices to Antenna object in pol.py

--- src/pol.py
+++ src/pol.py
@@ -38,6 +38,15 @@ class UV(miriad.UV):
 #  \___/ \__|_|_|_|\__|\__, | |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
 #                      |___/        
 
+p2i = {'x':0,'y':1} #indices for each polarization
+
+#Pauli Spin-Matrices
+Sigma = {'t': np.matrix([[1,0],[0,1]]).
+         'x': np.matrix([[0,1],[1,0]]),
+         'y': np.matrix([[0,-1.j],[1.j,0]]),
+         'z': np.matrix([[1,0],[0,-1]])}
+
+
 def ParAng(ha,dec,lat):
     """
     For any hour angle, declenation in an image, calculate the paralactic angle at that point. Remember to multiply this by 2 when you're
@@ -58,6 +67,25 @@ def ParAng(ha,dec,lat):
 #   / _ \ | '_ \| __/ _ \ '_ \| '_ \ / _` |
 #  / ___ \| | | | ||  __/ | | | | | | (_| |
 # /_/   \_\_| |_|\__\___|_| |_|_| |_|\__,_|
+
+class Antenna(a.fit.Antenna):
+    def __init__(self,x,y,z,beam,d=0., **kwargs)
+        pol.Antenna.__init__(self,x,y,z,beam,**kwargs)
+        self.d = d #I may want to update this to be a polynomial or something later (dfm)
+    def G_i(self):
+        """2x2 gain matrix"""
+        amp_i = self.passband()
+        phs_i = self.phsoff
+        g_ix = amp_i[0]*np.exp(-2.j*np.pi*phs_i[0])
+        g_iy = amp_i[1]*np.exp(-2.j*np.pi*phs_i[1])
+        return [np.array([[g_ix[i],0.],[0.,g_iy[i]]]) for i in range(len(g_ix))]
+    def D_i(self):
+        """2x2 rotation matrix for this antenna -- to first order in rot_angle."""
+        return np.array([[1.,self.d[1]],[-1.*np.conjugate(self.d[0]),1.]])
+    def J_i(self):
+        """Compute the Jones' matrix for this antenna."""
+        return [np.dot(G_i[i],D_i) for i in range(len(G_i))]
+
 
 
 #     _          n                            _                         
