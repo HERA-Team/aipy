d6f564e2b50907211e9bca7f787b3dff4c94e133
jaycedowell <jayce.dowell@gmail.com>
2018-06-11 13:13:33 -0600

    Oh, the real problem the the Helmboldt catalog was that setup.py was excluding it from the install.  This should fix that.

--- aipy_src/src.py
+++ aipy_src/src.py
@@ -1,8 +1,5 @@
 # Python3 compatibility
 from __future__ import print_function, division, absolute_import
-import sys
-if sys.version_info < (3,):
-    FileNotFoundError = IOError
 
 """
 This module provides a front-end interface for accessing sources in 
@@ -10,7 +7,6 @@ all catalogs in the _src module of AIPY.
 """
 
 from . import fit, _src
-import warnings
 
 def get_catalog(srcs=None, cutoff=None, catalogs=['helm','misc']):
     """Return a source catalog created out of the sources listed in 'srcs',
@@ -22,11 +18,9 @@ def get_catalog(srcs=None, cutoff=None, catalogs=['helm','misc']):
             c = getattr(_src, c)
         except(AttributeError):
             continue
-        try:
-            srclist += c.get_srcs(srcs=srcs, cutoff=cutoff)
-        except(FileNotFoundError) as e:
-            warnings.warn("Catalog - %s" % str(e), RuntimeWarning)
-            continue
+        
+        srclist += c.get_srcs(srcs=srcs, cutoff=cutoff)
+        
     # Add in sources that are already made
     if srcs != None: srclist += [s for s in srcs if type(s) != str]
     return fit.SrcCatalog(srclist)
