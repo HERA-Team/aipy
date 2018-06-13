b0e8802b09efc270785236264f0f31de3a28b11e
Aaron Parsons <aparsons@berkeley.edu>
2018-06-13 14:26:14 -0700

    Stripped out optimize and import scipy.optimize instead.

--- aipy_src/__init__.py
+++ aipy_src/__init__.py
@@ -13,7 +13,8 @@ Author: Aaron Parsons
 import phs, const, coord, deconv
 import ephem, fit, healpix, img
 import interp, cal, map, miriad
-import optimize, rfi, amp, scripting, src, _src, utils
+import scipy.optimize as optimize
+import rfi, amp, scripting, src, _src, utils
 import dsp
 import pol, twodgauss #added by dfm
 from __gitlog__ import __gitlog__
