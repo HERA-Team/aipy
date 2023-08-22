fc7238f3ba0305d38e3d0f43e1759536f03471a6
Josh Dillon <jsdillon@berkeley.edu>
2023-08-22 14:42:32 -0700

    fix numpy type deprecations

--- tests/matplotlib_tests/deconv1d_test.py
+++ tests/matplotlib_tests/deconv1d_test.py
@@ -5,8 +5,8 @@ from __future__ import print_function, division, absolute_import
 import aipy as a, numpy as n
 from matplotlib import pylab as p
 
-img = n.array([0,0,0,4,6,4,0,0,-2,-3,-2,0], dtype=n.float)
-ker = n.array([3,2,0,0,0,0,0,0,0,0,0,2], dtype=n.float)
+img = n.array([0,0,0,4,6,4,0,0,-2,-3,-2,0], dtype=float)
+ker = n.array([3,2,0,0,0,0,0,0,0,0,0,2], dtype=float)
 
 print('REAL TEST:')
 print('img', img)
@@ -16,8 +16,8 @@ print('cln', cln)
 print(info)
 print('-----------------------------------------------------------------')
 
-img = n.array([0,0,0,4j,6j,4j,0,0,2+2j,3+3j,2+2j,0], dtype=n.complex)
-ker = n.array([3,2,0,0,0,0,0,0,0,0,0,2], dtype=n.complex)
+img = n.array([0,0,0,4j,6j,4j,0,0,2+2j,3+3j,2+2j,0], dtype=complex)
+ker = n.array([3,2,0,0,0,0,0,0,0,0,0,2], dtype=complex)
 
 print('CMPLX TEST:')
 print('img', img)
@@ -28,10 +28,10 @@ print(info)
 print('-----------------------------------------------------------------')
 
 SIZE = 16
-img = n.zeros((SIZE,), n.complex)
+img = n.zeros((SIZE,), complex)
 img[5] = 1+2j
 img[10] = 3+4j
-ker = n.zeros((SIZE,), n.complex)
+ker = n.zeros((SIZE,), complex)
 ker[0] = 2j
 ker[1] = 1+1j; ker[-1] = -1-1j
 ker[2] = .5+.5j; ker[-2] = -.5-.5j
